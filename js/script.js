22bfed166b2fc165eebcc9fc58d9e66241dd566d
Madu Jang <madu.jang93@gmail.com>
2023-09-11 12:46:57 +0100

    corrections

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
-const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const myDate = new Date();
-let day = weekday[myDate.getDay()];
-let hours = myDate.getUTCHours();
-let mins = myDate.getUTCMinutes();
-let secs = myDate.getUTCSeconds();
-let millisecs = myDate.getUTCMilliseconds();
-let utcDate = hours + ":" + mins + ":" + secs + "." + millisecs;
+function refresh_time() {
+    let refresh = 1000;
+    mytime = setTimeout("curr_time()", refresh);
+}
 
-document.getElementById("currentDayOfTheWeek").innerHTML = "<strong>Day Of The Week: </strong>" + day;
-document.getElementById("currentUTCTime").innerHTML = "<strong>UTC Time: </strong>" + utcDate;
+function curr_time() {
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const myDate = new Date();
+    let day = weekday[myDate.getDay()];
+    let hours = myDate.getUTCHours();
+    let mins = myDate.getUTCMinutes();
+    let secs = myDate.getUTCSeconds();
+    let millisecs = myDate.getUTCMilliseconds();
+    let utcDate = hours + ":" + mins + ":" + secs + "." + millisecs;
+
+    document.getElementById("currentDayOfTheWeek").innerHTML = day;
+    document.getElementById("currentUTCTime").innerHTML = utcDate;
+
+    refresh_time();
+}
